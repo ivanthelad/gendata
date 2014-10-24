fe76f90c598866aa3c0ed8e96d2777dbebd90e70
Ivan Mc Kinley <imckinle@redhat.com>
2014-10-24 10:31:18 +0200

    gen data

--- sendMyRequestQueue.sh
+++ sendMyRequestQueue.sh
@@ -4,25 +4,31 @@ value=$RANDOM
 #value=15931
 tmpdir="tmpdir"
 #rm -rf $tmpdir
-Suites="updateContract lookUpContract provisionContract renewContract"
-faults="Business_fault Technical_fault Maintenance_fault"
+Suites="BUTTER JAM LAPTOP BUTTER CHEESE CHEESE"
+
+customers="Fred Fred Fred Fred Ivan Tom Joe Eric Ivan Gary Eric"
+
 #faults="Business_fault Technical_fault"
 
 #Suites="provisionContract renewContract"
 suite=($Suites)
 num_suites=${#suite[*]}
-operation_type=${suite[$((RANDOM%num_suites))]}
+product_type=${suite[$((RANDOM%num_suites))]}
 
-fault=($faults)
-num_fault=${#fault[*]}
-fault_type=${fault[$((RANDOM%num_fault))]}
+
+
+customer=($customers)
+num_customer=${#customer[*]}
+customer_type=${customer[$((RANDOM%num_customer))]}
 
 
 echo $operation_type
 
 mkdir $tmpdir
 filename="SampleRequest"
-url="http://127.0.0.1:8080/overlord-rtgov/activity/store"
+payment="payment"
+
+url="http://localhost:8080/demo-orders/OrderService"
 
 echo "********* [BEGIN] Simulating for generated id $value ************"
 
@@ -31,38 +37,43 @@ namer="ivanservice"
 echo $timer
 
 timer=`date +%s`
-file="$tmpdir/output.$filename.$value.json"
-tmpfile="$tmpdir/output.$filename.$value.json.tmp"
+file="$tmpdir/output.$filename.$value.xml"
+tmpfile="$tmpdir/output.$filename.$value.json.xml"
 cp ${filename}.json ${file}
-for i in {1..13}
-do
+
+file_pay="$tmpdir/output.$payment.$value.xml"
+tmpfile_pay="$tmpdir/output.$payment.$value.json.xml"
+cp ${payment}.json ${file_pay}
+
  # file="$tmpdir/output.$filename.$value.json"
    placeHolder="TIMESTAMP_REPLACE_${i}_"
   sed  "s/ID_REPLACE/${value}/g"  "${file}" | \
-  sed -e "s/${placeHolder}/${timer}/g" | sed -e "s/OPERATION_TYPE/${operation_type}/g"| sed -e "s/FAULT_REPLACE/${fault_type}/g"| sed -e "s/contracthandlervb/${namer}/g" > "${tmpfile}"
-  #curl -X POST -H "Content-Type: application/json" -d '{"username":"admin","password":"overlord"}' http://127.0.0.1:8080/overlord-rtgov/activity/store
-#curl -X POST -d "@${file}" "${url}"  --user fswAdmin:notruf678# --header "Content-Type:application/json"
-#let timer=$timer+300 ;
-#if [ "$i" -eq "12" ]; then 
-myDelay=$((RANDOM % 200))
+  sed -e "s/PRODUCT_REPLACE/${product_type}/g"|  sed -e "s/CUSTOMER_REPLACE/${customer_type}/g"  > "${tmpfile}"
+   sed  "s/ID_REPLACE/${value}/g"  "${file_pay}" | \
+    sed -e "s/PRODUCT_REPLACE/${product_type}/g"|  sed -e "s/CUSTOMER_REPLACE/${customer_type}/g"  > "${tmpfile_pay}"
+
+myDelay=$((RANDOM % 100))
 echo "******* Creating artificial delay of [${myDelay}] ms ******* "
 
 let timer=$timer+$myDelay
 #fi
-echo "Performing phase [$i] and operation ${operation_type} for conversationid=$value"
+echo "Performing phase [$i] and operation ${product_type} for conversationid=$value"
 cp ${tmpfile} ${file}
-done
+
 #sleep $((RANDOM % 10))
 
 
 echo "********* [END] Simulating for generated id $value ************"
 
-echo "********* [Sending]  for generated file id $value ************"
-curl -X POST -d "@${file}" "${url}"  --user fswAdmin:password --header "Content-Type:application/json"
+echo "********* [Sending] ${url} for generated file id $value,${file} ************"
+curl -X POST  -H "Content-Type: text/xml; charset=utf-8"   -d "@${file}" "${url}"  
 
+sleep 1
 
-rm ${tmpfile}
-rm ${file}
+curl -X POST  -H "Content-Type: text/xml; charset=utf-8"   -d "@${tmpfile_pay}" "${url}"  
+
+#rm ${tmpfile}
+#rm ${file}
 echo ""
 
 
